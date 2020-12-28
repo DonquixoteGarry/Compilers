24634ea6b55948376afd08e94f9e5934fcd82882
Donquixotegarry <zhengji20001205@gmail.com>
2020-12-28 23:30:24 +0800

    delete useless files

--- new_lab6/test/0.c
+++ new_lab6/test/0.c
@@ -1,19 +1,10 @@
-// int a = 1
-// int b = 2;
-// if(!2==3){
-//     int a=2;
-//     a=a+2;
-// }
-// while(a==b){
-//     printf("%d", a);
-// }
 void main()
 {
     int b = 10;
     if(!b)
-        printf("b");
+        printf("\nb\n");
     else
     {
-        printf("a");
+        printf("\na\n");
     }
 }
